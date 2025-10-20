b2c850218029388f232dd4ac36bc301db6ca5707
facutateo <facutateo@gmail.com>
2025-10-20 14:42:15 -0300

    better user experience

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,14 +8,14 @@ function Card ({ id, title, imageUrl,country,videoUrl }: cardProps) {
     return<>
     <div className="animate-fade-in-down border-2 border-gray-300 rounded-lg  shadow-md w-64 flex flex-col items-center gap-2 font-serif ">
         <img src={imageUrl} alt= "" loading="lazy" className="rounded-t-sm w-full"/>
-        <div >
-            <h2>{title}</h2>
+        <div className="p-2.5" >
+            <h2 className="text-center">{title}</h2>
         </div>
         <div className="justify-content-center">
             <p>{country}</p>
         </div>
         <div className="justify-between flex w-full items-center p-2.5">
-            <a href={videoUrl} target="_blank" className="bg-sky-300 rounded-lg p-1  hover:animate-pulsing hover:animate-iteration-count-infinite"> ver video</a>
+            <a href={videoUrl} target="_blank" className="bg-sky-300 rounded-lg p-1  hover:animate-pulsing hover:animate-iteration-count-infinite"> video </a>
             <button onClick={()=>onToggleClick(id)} className="hover:animate-scale">
                 <HeartIcon width={20} height={20} style={{fontSize: '20px', color: isfav? 'red' : 'white', stroke: isfav? 'red' : '#333', transition: 'all 0.5s', overflow: 'visible'}}/>
             </button>
